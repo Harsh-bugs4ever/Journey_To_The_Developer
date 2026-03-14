54142021b7fd026ff5c4104aed18a774b2748ab3
Harsh Singh <harshsinghathena@gmail.com>
2026-03-14 12:38:13 +0530

    Literals Data Types Casting & Conversion

---
+++ Literals.java
@@ -0,0 +1,15 @@
+public class Literals {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 0b101;
+        int num3 = 0X7E;
+        int num4 = 10_00_00_000;
+
+        double d1 = 56;
+        
+        char c = 'a';
+        c++;
+        System.out.println(num1+" "+num2+" "+num3+" "+num4+" "+d1+" "+c);
+    }
+    
+}
