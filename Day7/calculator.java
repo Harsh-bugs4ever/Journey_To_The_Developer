82c4d15629f44ea9f8f31688ac089e6fccbc8f2f
Harsh-bugs4ever <harshsinghathena@gmail.com>
2026-05-25 15:08:12 +0530

    Commit of class and object

---
+++ Day7/calculator.java
@@ -0,0 +1,14 @@
+class calc
+{
+    public int add()
+    {
+            System.out.print("in add");
+            return 0;
+    }
+}
+    public class calculator {
+        public static void main(String[] args) {
+            calc c1= new calc();
+           c1.add();
+        }
+}
